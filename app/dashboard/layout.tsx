0fc4f2541c16c744f94fd9b4de1c3a8535604d69
Bharath Sriraam R R <albharath.1305@gmail.com>
2024-05-14 16:11:00 -0400

    auth done

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,21 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect("/");
+  }
+
+  return children;
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { UserMenu } from "@/components/user-menu";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+
+      <UserMenu />
+    </div>
+  );
+};
+
+export default DashboardPage;
